039d8b7496d8e7073f3dd21c429c088e82abcc6f
jakeaetna <sandersonj1@aetna.com>
2023-03-23 15:52:49 -0600

    changes lazy row to be actual data for kicks and gigs

--- app/src/main/java/com/jakey/compose/presentation/players/PlayerViewModel.kt
+++ app/src/main/java/com/jakey/compose/presentation/players/PlayerViewModel.kt
@@ -9,7 +9,6 @@ import com.jakey.compose.domain.model.Player
 import com.jakey.compose.domain.repository.PlayersRepository
 import com.jakey.compose.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -22,8 +21,6 @@ class PlayerViewModel @Inject constructor(
 
     var state by mutableStateOf(PlayersUiState())
 
-    var query by mutableStateOf("asd")
-
     init {
         loadPlayers()
     }
@@ -32,7 +29,6 @@ class PlayerViewModel @Inject constructor(
         playersRepository.getPlayers().collectLatest {result ->
             Timber.tag("LoadPlayersViewModel").i("data: ${result.data} message: ${result.message}")
             state = state.copy(isLoading = true)
-            delay(500L)
             when (result) {
                 is Resource.Success -> {
                     state = state.copy(players = result.data ?: listOf(), isLoading = false)
